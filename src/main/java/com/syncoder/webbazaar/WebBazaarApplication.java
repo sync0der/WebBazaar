cee8b42d8345f395cb04b767311b695d1bb227fa
sync0der <ahmadjanovinbox@gmail.com>
2024-08-11 00:29:38 +0500

    initial commit

---
+++ src/main/java/com/syncoder/webbazaar/WebBazaarApplication.java
@@ -0,0 +1,13 @@
+package com.syncoder.webbazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBazaarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBazaarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/syncoder/webbazaar/WebBazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncoder.webbazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBazaarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
