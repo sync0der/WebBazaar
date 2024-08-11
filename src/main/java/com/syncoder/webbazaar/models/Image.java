daf469cc0b268a2ea26a225e502d03ad18d153cc
sync0der <ahmadjanovinbox@gmail.com>
2024-08-12 00:29:45 +0500

    create Image and Product tables and make one-to-many relationship between them.

---
+++ src/main/java/com/syncoder/webbazaar/models/Image.java
@@ -0,0 +1,34 @@
+package com.syncoder.webbazaar.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String originalFileName;
+
+    private Long size;
+
+    private String contentType;
+
+    private boolean isPreviewImage;
+
+    @Lob
+    private byte[] bytes;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private Product product;
+
+}
--- src/main/java/com/syncoder/webbazaar/models/Product.java
+++ src/main/java/com/syncoder/webbazaar/models/Product.java
@@ -1,9 +1,14 @@
 package com.syncoder.webbazaar.models;
 
+import com.sun.jdi.PrimitiveValue;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "products")
@@ -25,4 +30,16 @@ public class Product {
     private String city;
 
     private String author;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+
+    private Long previewImageId;
+
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    private void init() {
+        createdDate = LocalDateTime.now();
+    }
 }
